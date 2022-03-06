0c0dc82692b35edff907eeb06e3e4ea53577c2cf
Václav Ocelík <v.ocelik@uva.nl>
2022-03-06 09:38:11 +0100

    mistake in vars.py with weight and mass

--- vars.py
+++ vars.py
@@ -9,14 +9,14 @@ np.random.seed(1) # important because we are generating the mass distributions t
 
 regex = re.compile('[^a-zA-Z]')
 strategies = [axl.Defector, axl.TitForTat, axl.Cooperator, axl.WinStayLoseShift, axl.Adaptive, axl.Grudger, axl.ZDExtortion, axl.ZDGen2, axl.GTFT, axl.Bully]
-MAX_ROUNDS = 1000
+NUMBER_OF_PLAYERS = 5
+MAX_ROUNDS = 100
 SEEDS = [1,2,3,4,5,6,7,8,9,10]
 TURNS = 200
 MUTATION_RATE = .1
 NOISE = .1
 MASS_BASE = 1
 WEIGHT_BASE = 1
-NUMBER_OF_PLAYERS = 5
 PLAYERS = [player() for player in range(NUMBER_OF_PLAYERS) for player in strategies]
 random.Random(1).shuffle(PLAYERS)
 
@@ -45,5 +45,5 @@ distributions_mass = {
 distributions_weight = {
     "normal": [round(np.random.choice([i for i in W_trunc.rvs(1000)]), 1) for _ in range(len(PLAYERS))],
     "pareto": [round(np.random.choice(pareto_sample_truncated), 1) for _ in range(len(PLAYERS))],
-    "homo":[MASS_BASE for _ in range(len(PLAYERS))]
+    "homo":[WEIGHT_BASE for _ in range(len(PLAYERS))]
 }
