ae0fefdb4f60459fd36a17a6d3a349011cd94864
Václav Ocelík <v.ocelik@uva.nl>
2022-03-07 11:27:36 +0100

    added uniform distribution

--- vars.py
+++ vars.py
@@ -11,8 +11,8 @@ regex = re.compile('[^a-zA-Z]') # used for cleaning result output
 
 
 # MORAN PROCESS SETUP
-MAX_ROUNDS = 50
-SEEDS = [1,2,3,4,5,6,7,8,9,10]
+MAX_ROUNDS = 5
+SEEDS = [1,2,3,4,5]
 TURNS = 100
 MUTATION_RATE = .1
 NOISE = .1
@@ -40,6 +40,10 @@ normal_population_independence = np.random.normal(1, 1/3, pow(10,6))
 symetric_bimodal, symetric_prob = [.7, 1.3], [.5, .5]
 asymetric_bimodal, asymetric_prob = [.85, 1.6], [.8, .2]
 
+## Uniform
+uniform_population_mass = np.random.uniform(.1, 2, pow(10,6))
+uniform_population_independence = np.random.normal(.1, 2, pow(10,6))
+
 ## Homogenous
 
 mass_base = 1 # value of mass when distribution is homogenous
@@ -51,6 +55,7 @@ independence_base = 1 # value of independence when distribution is homogenous
 distributions_mass = {
     "normal": normal_population_mass[(normal_population_mass < normal_mass_upper_bound) & (normal_population_mass > normal_mass_lower_bound)][:len(PLAYERS)].round(2),
     "pareto": pareto_population_mass[pareto_population_mass < pareto_upper_bound_mass][:len(PLAYERS)].round(2),
+    "uniform": uniform_population_mass[:len(PLAYERS)].round(2),
     "symetric_bimodal": list(np.random.choice(symetric_bimodal, len(PLAYERS), p = symetric_prob)),
     "asymetric_bimodal": list(np.random.choice(asymetric_bimodal, len(PLAYERS), p = asymetric_prob)),
     "homo":[mass_base for _ in range(len(PLAYERS))]
@@ -60,6 +65,7 @@ distributions_mass = {
 distributions_independence = {
     "normal": normal_population_independence[(normal_population_independence < normal_independence_upper_bound) & (normal_population_independence > normal_independence_lower_bound)][:len(PLAYERS)].round(2),
     "pareto": pareto_population_independence[pareto_population_independence < pareto_upper_bound_independence][:len(PLAYERS)].round(2),
+    "uniform": uniform_population_independence[:len(PLAYERS)].round(2),
     "symetric_bimodal": list(np.random.choice(symetric_bimodal, len(PLAYERS), p = symetric_prob)),
     "asymetric_bimodal": list(np.random.choice(asymetric_bimodal, len(PLAYERS), p = asymetric_prob)),
     "homo":[independence_base for _ in range(len(PLAYERS))]
