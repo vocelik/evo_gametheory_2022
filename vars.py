d1a372750ed390cac124b489073f625e4c86500b
Sebastian Krapohl <s.krapohl@uva.nl>
2022-03-07 16:15:46 +0100

    parameters of the distributions

--- vars.py
+++ vars.py
@@ -3,7 +3,7 @@ import numpy as np
 import re
 from scipy.stats import truncnorm
 
-np.random.seed(1) # important because we are generating the mass distributions through numpy
+np.random.seed(2048) # important because we are generating the mass distributions through numpy
 
 
 regex = re.compile('[^a-zA-Z]') # used for cleaning result output
@@ -11,37 +11,37 @@ regex = re.compile('[^a-zA-Z]') # used for cleaning result output
 
 # MORAN PROCESS SETUP
 MAX_ROUNDS = 1000
-SEEDS = [i + 1 for i in range(10)]
+SEEDS = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
 TURNS = 100
 MUTATION_RATE = .1
 NOISE = .1
 
 # STRATEGY SETUP
-STRATEGIES = [axl.Defector, axl.WinStayLoseShift, axl.TitForTat, axl.GTFT, axl.Cooperator]
-NUMBER_OF_PLAYERS = 5
+STRATEGIES = [axl.Defector, axl.TitForTat, axl.GTFT, axl.Cooperator]
+NUMBER_OF_PLAYERS = 10
 PLAYERS = [player() for player in range(NUMBER_OF_PLAYERS) for player in STRATEGIES]
 np.random.shuffle(PLAYERS) # randomize list of players
 
 # DISTRIBUTIONS
 
 ## Pareto
-pareto_upper_bound_mass, pareto_upper_bound_independence = 2, 2
-pareto_population_mass = (np.random.pareto(1, pow(10,6)) + 1) * 1/10
-pareto_population_independence = (np.random.pareto(1, pow(10,6)) + 1) * 1/10
+pareto_upper_bound_mass, pareto_upper_bound_independence = 100, 100
+pareto_population_mass = (np.random.pareto(3.23, pow(10,6)) + 1) * .69
+pareto_population_independence = (np.random.pareto(3.23, pow(10,6)) + 1) * .69
 
 ## Normal
-normal_mass_lower_bound, normal_mass_upper_bound = 1/10, 2
-normal_population_mass = np.random.normal(1, 1/3, pow(10,6))
-normal_independence_lower_bound, normal_independence_upper_bound = .1, 2
-normal_population_independence = np.random.normal(1, 1/3, pow(10,6))
+normal_mass_lower_bound, normal_mass_upper_bound = .01, 1.99
+normal_population_mass = np.random.normal(1, .5, pow(10,6))
+normal_independence_lower_bound, normal_independence_upper_bound = .01, 1.99
+normal_population_independence = np.random.normal(1, 0.5, pow(10,6))
 
 ## Bimodal
-symetric_bimodal, symetric_prob = [.7, 1.3], [.5, .5]
-asymetric_bimodal, asymetric_prob = [.85, 1.6], [.8, .2]
+symetric_bimodal, symetric_prob = [.5, 1.5], [.5, .5]
+asymetric_bimodal, asymetric_prob = [.75, 2], [.8, .2]
 
 ## Uniform
-uniform_population_mass = np.random.uniform(.1, 2, pow(10,6))
-uniform_population_independence = np.random.normal(.1, 2, pow(10,6))
+uniform_population_mass = np.random.uniform(.13, 1.87, pow(10,6))
+uniform_population_independence = np.random.normal(.13, 1.87, pow(10,6))
 
 ## Homogenous
 
